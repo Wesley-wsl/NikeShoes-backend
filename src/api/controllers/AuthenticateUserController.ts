4d9f626b6568ca8f7fdca33f23c72512aaa93c46
Wesley Westelley <wesleywestelley@gmail.com>
2021-11-23 19:54:31 -0300

    :wrench: feat: controls for user authentication

---
+++ src/api/controllers/AuthenticateUserController.ts
@@ -0,0 +1,19 @@
+import { NextFunction, Request, Response } from "express";
+import AuthenticateUserService from "../services/AuthenticateUserService";
+
+export default {
+    async handle(request: Request, response: Response, next: NextFunction) {
+        try {
+            const { email, password } = request.body;
+
+            const token = await AuthenticateUserService.execute({
+                email,
+                password,
+            });
+
+            return response.json(token);
+        } catch (err) {
+            next(err);
+        }
+    },
+};
