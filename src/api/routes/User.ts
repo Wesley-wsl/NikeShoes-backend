a8d8d6a3251f4ef3a89a7f01b805e3d04eee4f59
Wesley Westelley <wesleywestelley@gmail.com>
2021-11-26 01:20:57 -0300

    :wrench: feat: route for delete user created.
    Route for delete user and middlewares for security added in application (middlewares: ensureAdmin, isValidId, ensureAuthenticated).

--- src/api/routes/User.ts
+++ src/api/routes/User.ts
@@ -1,11 +1,21 @@
-import express, { Request, Response } from "express";
+import express from "express";
 import AuthenticateUserController from "../controllers/AuthenticateUserController";
 import UserControllers from "../controllers/UserControllers";
+import { ensureAdmin } from "../middlewares/ensureAdmin";
+import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
+import isValidId from "../middlewares/isValidId";
 
 const routes = express.Router();
 
 routes.post("/", UserControllers.createNewUser);
 routes.post("/login", AuthenticateUserController.handle);
-routes.delete("/:id", UserControllers.deleteUserById);
+routes.delete(
+    "/:id",
+    ensureAuthenticated,
+    ensureAdmin,
+    isValidId,
+    UserControllers.deleteUserById
+);
+routes.get("/", ensureAuthenticated, UserControllers.listUsers);
 
 export { routes as UserRoutes };
