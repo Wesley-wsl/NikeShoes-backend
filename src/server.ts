279624a9f33fa8f3703bda1226ed31c00b3eee79
Wesley Westelley <wesleywestelley@gmail.com>
2021-11-26 01:16:47 -0300

    :wrench: feat: routes and handling error added

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,33 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
+import { ProductRoutes } from "./api/routes/Product";
+import { UserRoutes } from "./api/routes/User";
+import { CartRoutes } from "./api/routes/Cart";
+import cors from "cors";
+import "express-async-errors";
+import "./config/dbConfig";
 
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+app.use("/users", UserRoutes);
+app.use("/products", ProductRoutes);
+app.use("/cart", CartRoutes);
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    if (err instanceof Error) {
+        return res.status(400).json({
+            success: false,
+            error: err.message,
+        });
+    }
+
+    return res
+        .status(500)
+        .json({ status: "error", message: "Internal Server Error" });
+});
 
 app.listen(3333, () => console.log("Server is running in port 3333."));
 
