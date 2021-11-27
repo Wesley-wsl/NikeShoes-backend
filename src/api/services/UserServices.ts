c58f3106a19ed53422a8453a637bab77d7f901ba
Wesley Westelley <wesleywestelley@gmail.com>
2021-11-27 11:54:16 -0300

    :wrench: fet: services for users added

---
+++ src/api/services/UserServices.ts
@@ -0,0 +1,34 @@
+import UserModel from "../models/UserModel";
+import { IId, IUser } from "../@types";
+import { hash } from "bcryptjs";
+
+export default {
+    async createNewUser({ first_name, last_name, email, password, admin }: IUser) {
+        const userExists = await UserModel.findOne({ email });
+
+        if (userExists) throw new Error("User already registered");
+
+        const passwordHash = await hash(password as string, 8);
+
+        const createUser = await UserModel.create({
+            first_name,
+            last_name,
+            email,
+            password: passwordHash,
+            admin,
+        });
+
+        return createUser;
+    },
+
+    async deleteUserById({ id }: IId) {
+        const userDeleted = await UserModel.findByIdAndDelete({ _id: id });
+
+        return userDeleted;
+    },
+
+    async listUsers() {
+        const users = await UserModel.find({}).populate({path: "cart"});
+        return users;
+    },
+};
