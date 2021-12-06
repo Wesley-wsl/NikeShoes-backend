4f44c0aa8a525db7c12a22c9b234a30f665ec028
Wesley Westelley <wesleywestelley@gmail.com>
2021-12-06 17:05:42 -0300

    :wrench: feat: create services for cart

---
+++ src/api/services/CartServices.ts
@@ -0,0 +1,33 @@
+import { IId } from "../@types";
+import ProductModel from "../models/ProductModel";
+import UserModel from "../models/UserModel";
+
+export default {
+    async addProductInCart({ userId, id }: IId) {
+        const findProduct = await ProductModel.findById({ _id: id });
+
+        if (!findProduct) throw new Error("Product don't exists");
+
+        const findUser = await UserModel.findById({ _id: userId });
+
+        findUser.cart.push(id);
+        findUser.save();
+
+        return findProduct;
+    },
+
+    async removeProductFromCart({ userId, id }: IId) {
+        const findProduct = await ProductModel.findById({ _id: id });
+
+        if (!findProduct) throw new Error("Product don't exists");
+
+        const findUser = await UserModel.findById({ _id: userId });
+
+        findUser.cart.forEach((element: string, index: number) => {
+            if (element == id) return findUser.cart.splice(index, 1);
+        });
+        findUser.save();
+
+        return findProduct;
+    },
+};
