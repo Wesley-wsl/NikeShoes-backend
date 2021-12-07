5969411d2666ea67464e7d4d60350d6cb9482ae9
Wesley Westelley <wesleywestelley@gmail.com>
2021-12-07 19:19:36 -0300

    :test_tube: test: ensure that last_name is provided

--- src/tests/users.test.ts
+++ src/tests/users.test.ts
@@ -13,4 +13,15 @@ describe("createNewUser", () => {
         });
         expect(result.body.error).toEqual("first_name is required");
     });
+
+    test("return 400 if last_name is not provided", async () => {
+        const sut = request(app);
+        const result = await sut.post("/users").send({
+            first_name: "Rodrigo",
+            email: "rodrigovictor@gmail.com",
+            password: "1234",
+            admin: false,
+        });
+        expect(result.body.error).toEqual("last_name is required");
+    });
 });
