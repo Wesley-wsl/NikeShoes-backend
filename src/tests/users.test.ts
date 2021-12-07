21344d97fbeb22ab0ab39ee784af162a790a2443
Wesley Westelley <wesleywestelley@gmail.com>
2021-12-07 19:26:34 -0300

    :test_tube: test: ensure create new user

--- src/tests/users.test.ts
+++ src/tests/users.test.ts
@@ -12,6 +12,7 @@ describe("createNewUser", () => {
             admin: false,
         });
         expect(result.body.error).toEqual("first_name is required");
+        expect(result.statusCode).toBe(400);
     });
 
     test("return 400 if last_name is not provided", async () => {
@@ -23,6 +24,7 @@ describe("createNewUser", () => {
             admin: false,
         });
         expect(result.body.error).toEqual("last_name is required");
+        expect(result.statusCode).toBe(400);
     });
 
     test("return 400 if password is not provided", async () => {
@@ -34,6 +36,7 @@ describe("createNewUser", () => {
             admin: false,
         });
         expect(result.body.error).toEqual("password is required");
+        expect(result.statusCode).toBe(400);
     });
 
     test("return 400 if email is not provided", async () => {
@@ -45,5 +48,19 @@ describe("createNewUser", () => {
             admin: false,
         });
         expect(result.body.error).toEqual("email is required");
+        expect(result.statusCode).toBe(400);
+    });
+
+    test("should be able create a new user", async () => {
+        const sut = request(app);
+        const result = await sut.post("/users").send({
+            first_name: "Rodrigo",
+            last_name: "Victor'",
+            email: "rodrigovictor@gmail.com",
+            password: "1234",
+            admin: false,
+        });
+        expect(result.body.newUserCreated).toHaveProperty("_id");
+        expect(result.statusCode).toBe(201);
     });
 });
