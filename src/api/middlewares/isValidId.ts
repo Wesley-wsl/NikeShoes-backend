f24792e83de11abad9218503a45657595dd9e521
Wesley Westelley <wesleywestelley@gmail.com>
2021-12-08 20:07:02 -0300

    :recycle: refactor: export isValidId to single file

--- src/api/middlewares/isValidId.ts
+++ src/api/middlewares/isValidId.ts
@@ -1,6 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 
-export default async function isValidId(
+export async function isValidId(
     req: Request,
     res: Response,
     next: NextFunction,
