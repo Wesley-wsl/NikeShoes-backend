0cb5690cec401f46b1d9b19d62f694d02521195c
Wesley Westelley <wesleywestelley@gmail.com>
2021-12-19 13:16:16 -0300

    :wrench: chore: add video_url in schema

--- src/api/models/ProductModel.ts
+++ src/api/models/ProductModel.ts
@@ -6,6 +6,7 @@ const ProductModel = new mongoose.Schema({
     product_image: { type: String, required: true },
     category: { type: String, required: true },
     price: { type: Number, required: true },
+    video_url: { type: String, required: false },
 });
 
 export default mongoose.model("Product", ProductModel);
