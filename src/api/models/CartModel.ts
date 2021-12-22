8a52b5d28b29773b1892fdabb9205dc44e917079
Wesley Westelley <wesleywestelley@gmail.com>
2021-12-22 16:24:10 -0300

    :hammer: feat: add model for cart

---
+++ src/api/models/CartModel.ts
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const CartModel = new mongoose.Schema({
+    userId: {
+        type: String,
+        required: true,
+    },
+    productId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Product",
+        required: true,
+    },
+    quantity: { type: Number, required: true, default: 1 },
+});
+
+export default mongoose.model("Cart", CartModel);
