0962c80b6d8a311847f9a40c2c0f7be7437c52b6
Wesley Westelley <wesleywestelley@gmail.com>
2021-12-22 16:26:28 -0300

    :recycle: refactor: remove cart from UserModel

--- src/api/models/UserModel.ts
+++ src/api/models/UserModel.ts
@@ -5,14 +5,6 @@ const UserModel = new mongoose.Schema({
     last_name: { type: String, required: true },
     email: { type: String, required: true },
     password: { type: String, required: true },
-    cart: [
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "Product",
-            required: false,
-            default: [],
-        },
-    ],
     admin: { type: Boolean, required: true, default: false },
 });
 
