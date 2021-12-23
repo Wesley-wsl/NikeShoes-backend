c33055713495a865135c462507277569d79c26c9
Wesley Westelley <wesleywestelley@gmail.com>
2021-12-23 16:27:16 -0300

    :white_check_mark: feat: api integration with stripe

--- src/api/routes/Stripe.ts
+++ src/api/routes/Stripe.ts
@@ -1,6 +1,10 @@
 import dotenv from "dotenv";
 import express, { Request, Response } from "express";
 
+import { IPriceDate, IProductsStripe } from "../@types";
+import { ensureAuthenticated } from "../middlewares";
+import CartModel from "../models/CartModel";
+
 dotenv.config();
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
@@ -8,27 +12,44 @@ const stripe = require("stripe")(process.env.STRIPE_PRIVATE_KEY);
 
 const routes = express.Router();
 
-routes.post("/", async (req: Request, res: Response) => {
-    const { product } = req.body;
+routes.post("/", ensureAuthenticated, async (req: Request, res: Response) => {
+    const { userId }: any = req?.res?.locals;
+
+    const products: IProductsStripe[] = [];
+
+    const userCart = await CartModel.find({ userId: userId }).populate({
+        path: "productId",
+    });
+
+    userCart.forEach(
+        (element: { productId: IProductsStripe[]; quantity: number }) => {
+            element.productId[0].quantity = element.quantity;
+            products.push(element.productId[0]);
+        },
+    );
+
+    const items: IPriceDate[] = [];
+
+    products.forEach(product => {
+        items.push({
+            price_data: {
+                currency: "usd",
+                product_data: {
+                    name: product.name,
+                    images: [product.product_image],
+                },
+                unit_amount: product.price * 100,
+            },
+            quantity: product.quantity,
+        });
+    });
 
     const session = await stripe.checkout.sessions.create({
         payment_method_types: ["card"],
-        line_items: [
-            {
-                price_data: {
-                    currency: "usd",
-                    product_data: {
-                        name: product.name,
-                        images: [product.image],
-                    },
-                    unit_amount: product.amount * 100,
-                },
-                quantity: product.quantity,
-            },
-        ],
+        line_items: [...items],
         mode: "payment",
-        success_url: `http://localhost:3333/success.html`,
-        cancel_url: `http://localhost:3333/cancel.html`,
+        success_url: `http://localhost:3000/`,
+        cancel_url: `http://localhost:3000/cart`,
     });
 
     res.json({ url: session.url });
