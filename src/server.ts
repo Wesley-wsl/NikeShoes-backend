22e64e6b8edf084d7ec236d236ee4957d5a21b9e
Wesley Westelley <wesleywestelley@gmail.com>
2021-12-27 16:47:29 -0300

    :wrench: feat: add new router for documentation api

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
 import cors from "cors";
 import express, { NextFunction, Request, Response } from "express";
+import swaggerUi from "swagger-ui-express";
 
 import { CartRoutes, ProductRoutes, Stripe, UserRoutes } from "./api/routes";
+import swaggerJSON from "./config/swagger/index.json";
 
 import "express-async-errors";
 import "./config/dbConfig";
@@ -12,6 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerJSON));
 app.use("/users", UserRoutes);
 app.use("/products", ProductRoutes);
 app.use("/cart", CartRoutes);
